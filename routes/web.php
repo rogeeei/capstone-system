3fa3023c142cb604693a86c4930b59dcbbe0f4aa
unknown <cuevasrogelyn2@gmail.com>
2025-04-22 05:13:50 +0800

    fix: remove Fruitcake CORS and use Laravel default

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/fix-cors', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    return 'All caches cleared!';
 });
+
