74e6fab72bfb84db0441a2947094a6cd1fa55197
unknown <cuevasrogelyn2@gmail.com>
2025-04-21 13:50:49 +0800

    Fix CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://healthy-barrio-final.vercel.app','https://capstone-system-production-7c08.up.railway.app/'],
+    'allowed_origins' => ['https://healthy-barrio-final.vercel.app','https://capstone-system-production-7c08.up.railway.app'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
