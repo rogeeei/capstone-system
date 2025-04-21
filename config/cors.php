d2d5b3d09b7d25f44fea10c2175d90b2d917d93e
unknown <cuevasrogelyn2@gmail.com>
2025-04-21 11:21:22 +0800

    Fix port and CORS settings

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,9 @@
 <?php
 
-
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://healthy-barrio-final.vercel.app'],
-    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
