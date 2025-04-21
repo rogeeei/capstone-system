4bbaec1bca74d4f950d0028eb3d4b4a490f4819f
unknown <cuevasrogelyn2@gmail.com>
2025-04-22 04:19:28 +0800

    Fix CORS error

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Fruitcake\Cors\HandleCors; 
 
 
 
@@ -23,6 +24,7 @@ class Kernel extends HttpKernel
     \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
     \App\Http\Middleware\TrimStrings::class,
     \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    \Fruitcake\Cors\HandleCors::class, 
 ];
 
 
