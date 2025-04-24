96e6a2b0d582afc40c4b40ba316b589e7ecdf045
unknown <cuevasrogelyn2@gmail.com>
2025-04-25 03:48:03 +0800

    Fix CORS error

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://healthy-barrio-final.vercel.app',],
+    'allowed_origins' => ['https://healthy-barrio-final.vercel.app','healthy-barrio-final-rogeeeis-projects.vercel.app',
+'healthy-barrio-final-git-main-rogeeeis-projects.vercel.app'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
