d58013a170a71f3e26e3c5a730cbac2fc9bbed21
unknown <cuevasrogelyn2@gmail.com>
2025-04-28 21:41:28 +0800

    change cors url

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-      '*'
+      'https://healthy-barrio.vercel.app',
+      'https://healthy-barrio-git-main-rogeeeis-projects.vercel.app',
+      'https://healthy-barrio-475zlgjhj-rogeeeis-projects.vercel.app'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
