adb22267d4dba4c4c1eba80b5ccb614d1f4f6594
unknown <cuevasrogelyn2@gmail.com>
2025-04-28 17:06:12 +0800

    Fix: Update login handling and response error handling

--- config/cors.php
+++ config/cors.php
@@ -5,9 +5,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'], // Keep these
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'https://healthy-barrio-final.vercel.app',
-        'https://healthy-barrio-final-rogeeeis-projects.vercel.app',
-        'https://healthy-barrio-final-git-main-rogeeeis-projects.vercel.app',
+        'https://healthy-barrio.vercel.app/',
+        'https://healthy-barrio-git-main-rogeeeis-projects.vercel.app/',
+        'https://healthy-barrio-rogeeeis-projects.vercel.app/',
         'http://127.0.0.1:5500', // <-- remove the '/' at the end
     ],
     'allowed_origins_patterns' => [],
