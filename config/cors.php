2540e462be1decc3860b162ac2f653ba05d100ab
unknown <cuevasrogelyn2@gmail.com>
2025-04-28 17:22:24 +0800

    Redeploy the frontend

--- config/cors.php
+++ config/cors.php
@@ -2,17 +2,17 @@
 
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], // Keep these
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], 
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-    'https://healthy-barrio.vercel.app', // Remove the trailing slash
-    'https://healthy-barrio-git-main-rogeeeis-projects.vercel.app', // Remove the trailing slash
-    'https://healthy-barrio-rogeeeis-projects.vercel.app', // Remove the trailing slash
-    'http://127.0.0.1:5500', // <-- Keep this as is
+    'https://healthy-barrio.vercel.app', 
+    'https://healthy-barrio-7yy6i5f1n-rogeeeis-projects.vercel.app/', 
+    'healthy-barrio-git-main-rogeeeis-projects.vercel.app', 
+    'http://127.0.0.1:5500', 
 ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // <-- IMPORTANT!! set to true
+    'supports_credentials' => true, 
 ];
